32b7612657514d3d54c86af0bde3968701bfbb91
Golib Khalimov <Fredwared@gmail.com>
2021-08-14 13:20:04 +0500

    Models

---
+++ app/Models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Class User
+ * @package App\Models
+ */
+class User extends BaseModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'users';
+}
