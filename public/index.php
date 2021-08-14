08b8142514b0b54df4071e8d92918925350ef6a9
Golib Khalimov <Fredwared@gmail.com>
2021-08-14 13:17:09 +0500

    csrf generation

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['csrf']) || (isset($_SESSION['csrf-expire']) && time() > $_SESSION['csrf-expire'])) {
+    $_SESSION['csrf'] = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
+    $_SESSION['csrf-expire'] = time() + 3600;
+}
